1024ba92c61823175bdde71191d92c94a27f472e
James Koval <jediknight304@gmail.com>
2011-04-14 22:47:18 -0500

    Works by finding all food caves that have food producing loops, then finding paths to the food caves from the start, and from the food caves to the end. This is final project submission and I think breadth first would have been more efficient to use for parts of the solution

--- cave.lisp
+++ cave.lisp
@@ -1,7 +1,8 @@
 #!/usr/bin/clisp
-;Authors: James, Michael, Vladimir
+;Authors: James
 ;Purpose: Find the fastest exit out of a cave system using breadth-first
-;Copyright 2011 James Koval, Michael, and Vladimir
+;Copyright 2011 James Koval
+;License: GNU GPL v3+
 
 ;Nodes are kept in a hash table
 (defparameter caves (make-hash-table))
@@ -29,7 +30,7 @@
   (cave 'pit-jiln :children '(i l j n) :costs '(2 2) :att 'pit)
   (cave 'troll-pit :children '(pit-jiln n) :att 'troll)
   (cave 'food-pit :children '(pit-treasure) :att 'food)
-  (cave 'food-start :children '(start f k) :att 'food)
+  (cave 'food-start :children '(f k start) :att 'food)
   (cave 'k :children '(food-start j))
   (cave 'i :children '(pit-jiln pit-fil treasure) :costs '(2))
   (cave 'l :children '(pit-jiln pit-fil m) :costs '(2))
@@ -41,16 +42,26 @@
 (defun cave (name &key children (att 'harmless) costs)
 "A cave is made of a name: hash key, mapped to a value of this format:
    (children-list of connected cave names)
-     start/end/troll/food/pit/harmless-enum    explored/unexplored-enum
+     start/end/troll/food/pit/harmless-enum
      costs-parallel to children list, containing costs that aren't 1
+     unexplored/explored = nil/t
  This function uses optional parameters: children att costs
  att has the default value of harmless
 "
-  (setf (gethash name caves) (list children att costs)))
+  (setf (gethash name caves) (list children att costs nil)))
 
 (defun get-info (name)
 "Get the info for a cave"
   (gethash name caves))
+
+(defun set-explored (name)
+"Sets explored to true"
+  (setf (fourth (gethash name caves)) t))
+
+(defun reset-explored ()
+"Sets all caves to unexplored"
+  (loop for name being the hash-keys of caves do
+    (setf (fourth (gethash name caves)) nil)))
 
 (defun get-children (name)
 "return all children of a cave"
@@ -70,6 +81,10 @@
   (unless found (return-from get-cost 1))
   (unless (nth i costs) (return-from get-cost 1))
   (nth i costs))
+
+(defun is-explored (name)
+"Return true when the cave has been explored"
+  (fourth (get-info name)))
 
 (defun get-att (name)
 "Get the attribute of a cave"
@@ -119,24 +134,30 @@
     (when (has-food name) (push name foods)))
   foods)
 
-;TODO: Implement explored portions on a parameter
-(defun depth (name &optional food (steps 0) &key target)
-  (print (list name food steps));debug
-  (when (or (and (equal name target) target) 
-               (and (not (target)) (is-end name))) 
-    (return-from depth (list name)));found treasure
-  (when (has-troll name) (return-from depth nil));death by troll
-  (when food (when (< food 0) (return-from depth nil)));starve
-  (when (> steps 14) (return-from depth nil));exhastiou
-  ;not taking more than 15 steps is another hack to limit the search domain,
-  ; such that the algorithm can finish within a second
 
-  (when (has-pit name) (decf food 5))
-  (when (has-food name) (incf food 5))
+(defparameter can-pick-self t)
+
+(defun depth (name &optional (food 99999999) &key target reexplore)
+"Depth first search between caves: name and target. If target is nil, any end cave will be a target
+Food is given as an integer, or left out to assume infinite food
+reexplore is a boolean to declare whether the algorithm is allowed to reexplore previously
+  explored caves. Leaving it out assumes nil.
+Returns a path from name to target"
+  ;When we hit the end, or a specific target (when target is set), finish
+  (when (and can-pick-self (or (and (equal name target) target) 
+               (and (not target) (is-end name))))
+    (reset-explored)(return-from depth (list name)));found treasure
+  (when (and (not reexplore)(is-explored name)) (return-from depth nil));no need to reexplore
+  (set-explored name)
+  (when (has-troll name) (return-from depth nil));death by troll
+  (when (and food (< food 0)) (return-from depth nil));starve
+  (when (has-pit name) (decf food 5));fall in a pit and loose 5 food
+  (when (and can-pick-self (has-food name)) (incf food 5));find 5 food in a food cave
+  (setf can-pick-self t)
   (setf children (get-children name))
   (loop for child in children do
-    (setf answer (depth child (- food (get-cost name child)) (+ steps 1)))
-    (when (and answer (> food 0)) (return-from depth (append answer (list name)))))
+    (setf answer (depth child (- food (get-cost name child)) :target target :reexplore reexplore))
+    (when answer (return-from depth (append answer (list name)))))
           ;requiring more than 2 food is a hack specific to Tom's big cave,
           ;to force the algorithm to find the infinite food loop before exiting
   nil)
@@ -146,18 +167,21 @@
   (init)
 
   (setf initial-food 5)
+  (setf start-cave (get-start))
+
+  (defparameter food-paths (make-hash-table))
 
   ;Get the food caves that have loops back to themselves
   ;These loops must produce more food than they comsume
   (setf foods (get-food-caves))
-  (setf loop-foods nil)
   (loop for name in foods do
-    (when (depth name initial-food :target name) (push name loop-foods)))
+    (setf can-pick-self nil)
+    (when (setf path (depth name (- initial-food 1) :target name :reexplore t)) 
+      (push path (gethash name food-paths))))
 
   ;Find path from start to food
-  (defparameter food-paths (make-hash-table))
-  (loop for name in loop-foods do
-    (when (setf path (depth (get-start) initial-food :target name))
+  (loop for name being the hash-keys of food-paths do
+    (when (setf path (depth start-cave initial-food :target name :reexplore t))
       (push path (gethash name food-paths))))
 
   ;Find path from food cave to end. Make this breadth first, or implement explored caves
@@ -170,10 +194,11 @@
   (if successful-caves
     (progn
     (print "Found infinite food paths")
+    (print "((path between end and food) (path between start and food) (path from food to itself))")
     (loop for name in successful-caves do
       (print (gethash name food-paths))))
     ;else
-    (if (setf path (depth (get-start) initial-food))
+    (if (setf path (depth start-cave initial-food))
       (progn
       (print "Found normal path")
       (print path))
