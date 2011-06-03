6c599598e03f72f7f216a7cf6ff9b3de7cc6e492
James Koval <james.ross.koval@gmail.com>
2011-06-03 01:24:09 -0500

    Added more information about gnumon, including descriptions and moves

---
+++ gnumon/type_strengths.js
@@ -0,0 +1,53 @@
+var gnutype = {
+Bug:{
+  Good:['Grass','Psychic', 'Dark'],
+  Bad: ['Fire', 'Fighting', 'Poison', 'Flying', 'Ghost', 'Steel']},
+Dark :{
+  Good:['Ghost','Psychic'],
+  Bad:['Fighting','Dark Steel']},
+Dragon :{
+  Good:[' Dragon'],
+  Bad:[' Ice','Steel']},
+Electric :{
+  Good:['Water','Flying'],
+  Bad:['Grass','Electric','Ground','Dragon']},
+Fighting :{
+  Good:['Normal','Ice','Rock','Dark','Steel'],
+  Bad:['Poison','Flying','Bug','Ghost ','Psychic']},
+Fire :{
+  Good:['Grass','Ice','Bug','Steel'],
+  Bad:['Water','Fire','Dragon','Rock']},
+Flying :{
+  Good:['Grass','Fighting','Bug'],
+  Bad:['Electric','Rock','Steel']},
+Ghost :{
+  Good:['Ghost','Psychic'],
+  Bad:['Dark','Steel']},
+Grass :{
+  Good:['Water','Ground','Rock'],
+  Bad:['Fire','Grass','Poison','Flying','Bug','Dragon','Steel']},
+Ground :{
+  Good:['Fire','Poison','Electric','Rock','Steel'],
+  Bad:['Grass','Flying','Bug']},
+Ice :{
+  Good:['Grass','Ground','Flying','Dragon'],
+  Bad:['Water','Ice','Fire','Steel']},
+Normal :{
+  Good:[],
+  Bad:[' Fighting','Ghost','Rock','Steel']},
+Poison :{
+  Good:['Grass'],
+  Bad:['Poison','Ground','Rock','Ghost','Steel']},
+Psychic :{
+  Good:['Fighting','Poison'],
+  Bad:['Psychic','Dark','Steel']},
+Rock :{
+  Good:['Ice','Fire','Flying','Bug'],
+  Bad:['Fighting','Ground','Steel']},
+Steel :{
+  Good:['Ice','Rock'],
+  Bad:['Water','Fire','Water','Steel']},
+Water :{
+  Good:[' Fire','Ground','Rock'],
+  Bad:[' Water','Grass','Dragon']}
+}
