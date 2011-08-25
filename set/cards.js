4b44cba180a5e2699525735c47361a988d04b84d
James Koval <James.Ross.Koval@gmail.com>
2011-08-25 03:56:48 +0000

    Starting to work on board drawing using percentages of screen width and height so it scales nicely for android phone or 1080p screen

---
+++ set/cards.js
@@ -0,0 +1,25 @@
+var cards = {
+  init:function (){
+    this.deck = [];
+    for(var number = 0; number <=2; number++)
+    for(var symbol = 0; symbol <=2; symbol++)
+    for(var shading = 0; shading <=2; shading++)
+    for(var color = 0; color <=2; color++)
+      this.deck.push(new card({number:number,symbol:symbol,shading:shading,color:color}));
+  },
+  draw_card:function(){
+    deck[Math.random()*deck.length].location = 'play';
+  },
+  paint:function(g){
+    var board = [];
+    for(var card in this.deck)
+      if(card.location == 'play')
+        board.push(card);
+    card.width = 1/3;
+    card.height = 1/4;
+    var i = 0;
+    for(var x = 0; x < 3; x++)
+      for(var y = 0; y < 4; y++)
+        board[i++].draw(g,x/3,y/4);
+  },
+}
