33bbff55cb793245e5a1a214c369daede610354b
James Koval <james.ross.koval@gmail.com>
2013-01-22 00:23:49 -0800

    uppercase git HEAD

--- .bashrc.d/general.sh
+++ .bashrc.d/general.sh
@@ -24,7 +24,7 @@ alias gds='git diff --staged --color -w'
 alias gd='git diff --color -w'
 alias gl='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative --first-parent'
 alias glm='gl --author=koval'
-alias gph='git push origin head'
+alias gph='git push origin HEAD'
 alias gsh='git show --date=relative --color'
 alias flushdns="dscacheutil -flushcache"
 alias avg="awk '{a=a+\$1}END{print a/NR}';"
