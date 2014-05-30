9e0b750a613993cb612b9505bc352ec909a5f169
James Koval <James.Ross.Koval@gmail.com>
2014-05-30 14:11:32 -0700

    Update twitter_quoted_rt_fix.js

--- twitter_quoted_rt_fix.js
+++ twitter_quoted_rt_fix.js
@@ -11,5 +11,5 @@ replies = document.querySelectorAll('.stream-items .tweet-text')
 for(i = 0; i < replies.length; i++) {
   reply = replies[i]
   // delete all text inside the angled quotes, typical of quoted RT's
-  reply.innerHTML = reply.innerHTML.replace(/“.+?”/, '')
+  reply.innerHTML = reply.innerHTML.replace(/“[\S\s]+?”/, '')
 }
